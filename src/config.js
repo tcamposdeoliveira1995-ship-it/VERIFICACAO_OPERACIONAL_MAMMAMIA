304693e6ec93653092d80e9a82db47f3c8b2591e
THALITA CAMPOS DE OLIVEIRA <tcamposdeoliveira1995@gmail.com>
2026-07-02 17:45:39 -0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export const API_URL = 'https://script.google.com/macros/s/AKfycbwdde1LNBa6kgs_fNpukyd_yZxpv4GeORT6i4VjXhiy4aIv3-qY-3gkDDXcPdqHo1na/exec';
+
+export const EMPRESAS = ['TC', 'YUKA'];
+
+export const ITENS_PADRAO = [
+  { numero: 1, nome: 'Higienização das instalações, equipamentos móveis e utensílios' },
+  { numero: 2, nome: 'Controle da potabilidade da água' },
+  { numero: 3, nome: 'Higiene e saúde dos manipuladores' },
+  { numero: 4, nome: 'Manejo de resíduos' },
+  { numero: 5, nome: 'Manutenção preventiva e calibração de equipamentos' },
+  { numero: 6, nome: 'Controle integrado de vetores e pragas urbanas' },
+  { numero: 7, nome: 'Seleção de matérias primas/ingredientes e embalagens' },
+  { numero: 8, nome: 'Programa de recolhimento de alimentos' },
+  { numero: 9, nome: 'Liberação de produto e produto não conforme' },
+  { numero: 10, nome: 'Vestiários e sanitários' },
+  { numero: 11, nome: 'Iluminação' },
+  { numero: 12, nome: 'Ventilação' },
+  { numero: 13, nome: 'Análises laboratoriais' },
+  { numero: 14, nome: 'Controle de temperaturas' },
+  { numero: 15, nome: 'Procedimento sanitário operacional' },
+  { numero: 16, nome: 'Monitoramento' }
+];
